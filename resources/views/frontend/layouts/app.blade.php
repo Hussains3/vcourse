ffa3e6cc3cbc939c2a9187e3f8ee572956285f8c
Hussains3 <shussain8978@gmail.com>
2022-08-16 13:02:40 +0600

    Added meta tag

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -17,6 +17,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="p:domain_verify" content="8ed33929088c1c904d8fa9b1220f495c"/>
     <title>Vcourse</title>
     <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
 
