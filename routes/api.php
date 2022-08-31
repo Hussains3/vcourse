9f7b9cd4144274967b0d06140622cecfd2d6ef68
Hussains3 <shussain8978@gmail.com>
2022-08-31 15:08:15 +0600

    Course list and show api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('courses', [CourseController::class, 'index']);
+Route::get('/{course}/show', [CourseController::class, 'show']);
